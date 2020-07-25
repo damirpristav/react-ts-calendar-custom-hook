896b88b3a699de25ad105ef682df1831f5e3c2cb
Damir Pristav <damirpristav@gmail.com>
2020-07-25 09:33:33 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC, Fragment } from 'react';
 import './App.css';
 
-function App() {
+import Calendar from './components/Calendar';
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title has-text-centered">Calendar</h1>
+          </div>
+        </div>
+      </section>
+      <div className="container has-text-centered">
+        <Calendar />
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Calendar.tsx
@@ -0,0 +1,46 @@
+import React, { Fragment, FC } from 'react';
+
+import useCalendar, { Column } from '../hooks/useCalendar';
+
+const Calendar: FC = () => {
+  const { calendarRows, selectedDate, todayFormatted, daysShort, monthNames, getNextMonth, getPrevMonth } = useCalendar();
+
+  const dateClickHandler = (date: string) => {
+    console.log(date);
+  }
+
+  return(
+    <Fragment>
+      <p>Selected Month: {`${monthNames[selectedDate.getMonth()]} - ${selectedDate.getFullYear()}`}</p>
+      <table className="table">
+        <thead>
+          <tr>
+            {daysShort.map(day => (
+              <th key={day}>{day}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Object.values(calendarRows).map((cols: Column[]) => {
+              return <tr key={cols[0].date}>
+                {cols.map(col => (
+                  col.date === todayFormatted
+                    ? <td key={col.date} className={`${col.classes} today`} onClick={() => dateClickHandler(col.date)}>
+                      {col.value}
+                    </td>
+                    : <td key={col.date} className={col.classes} onClick={() => dateClickHandler(col.date)}>{col.value}</td>
+                ))}
+              </tr>
+            })
+          }
+        </tbody>
+      </table>
+
+      <button className="button" onClick={getPrevMonth}>Prev</button>
+      <button className="button" onClick={getNextMonth}>Next</button>
+    </Fragment>
+  );
+}
+
+export default Calendar;
